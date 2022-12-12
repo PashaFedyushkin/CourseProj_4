f5ee0ecd1b96af1b98deb5e8d07de5f7549ffae1
PashaFedyushkin <pashafedyushkin@yandex.by>
2022-12-12 18:46:28 +0300

    final store

--- my-shop/src/pages/bookpage/bookpage.js
+++ my-shop/src/pages/bookpage/bookpage.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { BookBuy } from "../../components/bookbuy";
+import { BookCover } from "../../components/bookcover";
+import { BookGenre } from "../../components/bookgenre";
 import './bookpage.css';
 
 export const BookPage = () =>{
+  const book = useSelector(state => state.book.currentBook);
+  if (!book) return null ;
     return(
         <div>
-          asd
+          <h1>
+            {book.title}
+          </h1>
+          <div className="bookpage-content">
+            <div className="bookpage-left">
+              <iframe width="90%" height="400px" src = {book.video} title = "Yutube audio book"></iframe>
+            </div>
+            <div className="bookpage-right">
+              <BookCover image = {book.image}/>
+              <p>{book.description}</p>
+              {book.genres.map((genre) => <BookGenre genre = {genre} key = {genre}/>)}
+              <div className="bookpage-buy-book">
+                <BookBuy book = {book}/>              
+              </div>
+            </div>
+          </div>
         </div>
     )
 }
